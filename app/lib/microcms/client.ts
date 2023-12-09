9078e277790ab7ec2cf8842a3d84d69364fb71d7
渥美雄太 <atsumiyuuta@ayMacBook-Air.local>
2023-12-09 10:46:40 +0900

    mod3

--- app/lib/microcms/client.ts
+++ app/lib/microcms/client.ts
@@ -9,6 +9,9 @@ export const client = createClient({
 export const getAllBooks = async () => {
   const allBooks = await client.getList<BookType>({
     endpoint: "bookcommerce",
+    customRequestInit: {
+      cache: "no-store",
+    },
   });
 
   return allBooks;
@@ -19,7 +22,9 @@ export const getDetailBook = async (contentId: string) => {
   const detailBook = await client.getListDetail<BookType>({
     endpoint:"bookcommerce",
     contentId,
+    customRequestInit: {
+      cache: "no-store",
+    },
   });
-
   return detailBook;
 };
