c1353a9ca6902a0daab688ba407584cb36331f02
渥美雄太 <atsumiyuuta@ayMacBook-Air.local>
2023-12-09 10:55:36 +0900

    mod3

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -94,9 +94,9 @@ const Book = ({ book, isPurchased }: BookProps) => {
             priority
             src={book.thumbnail.url}
             alt={book.title}
-            width={400}
-            height={300}
-            className="rounded-t-md"
+            width={450}
+            height={350}
+            className="rounded-t-md object-cover"
           />
           <div className="px-4 py-4 bg-slate-100 rounded-b-md">
             <h2 className="text-lg font-semibold">{book.title}</h2>
